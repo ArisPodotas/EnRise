dd0d16cdbcc3984863c835b9c3bd91459766acdb
Aris Podotas <a.Podotas@gmail.com>
2024-08-10 17:08:04 +0300

    moved the icons for the schocial media to be the same order in the header and footer. Added the about us news, articles, organisation and vision pages

---
+++ index.js
@@ -0,0 +1,15 @@
+
+const attempt = document.getElementsByClassName('form');
+const qr = document.getElementsByClassName('qr');
+
+function forforms(){
+    if (){}
+    else{break;}
+}
+
+function main(){forforms()}
+
+if(name == 'main'){
+    main()
+}
+
