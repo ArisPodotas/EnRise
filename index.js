45b3531e62ece72747041159cddcac1dc1c1bfb8
Aris Podotas <a.Podotas@gmail.com>
2024-08-18 23:46:24 +0300

    changed header to have tabs, adde dt hesymbol abobe it, added spin animation to logo added all the animatios buttons heading text links, moved the new header to all files

--- index.js
+++ index.js
@@ -1,15 +1,11 @@
-
-const attempt = document.getElementsByClassName('form');
-const qr = document.getElementsByClassName('qr');
-
-function forforms(){
-    if (){}
-    else{break;}
-}
-
-function main(){forforms()}
-
-if(name == 'main'){
-    main()
-}
+window.addEventListener('scroll', function() {
+    const item = document.getElementsByClassName('btn')[0]; // [0] to select and element object from the list
+    const scrollPosition = window.scrollY + window.innerHeight; // How much the user has scrolled (from the top)
+    const documentHeight = document.documentElement.scrollHeight; // Total page height
+    if (scrollPosition >= documentHeight * 0.90) {
+        item.classList.remove('hidden');
+    } else {
+        item.classList.add('hidden'); // Optional: to hide the element again when scrolling back up
+    }
+});
 
